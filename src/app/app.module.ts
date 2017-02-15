81dbdb21330c6b81204344831d6f4731bf48f49e
Hetty de Vries <hetty82@gmail.com>
2017-02-15 17:43:01 +0100

    Services and Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,31 @@ import { BrowserModule }         from '@angular/platform-browser';
 import { FormsModule }           from '@angular/forms';
 
 import { AppComponent }          from './app.component';
+import { DashboardComponent }    from './dashboard.component';
+import { HeroesComponent }       from './heroes.component';
 import { HeroDetailComponent }   from './hero-detail.component';
+
+import { HeroService }           from './hero.service';
+
+import { AppRoutingModule }      from './app-routing.module';
+
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
+    AppRoutingModule,
   ],
   declarations: [
     AppComponent,
+    DashboardComponent,
+    HeroesComponent,
     HeroDetailComponent,
+  ],
+  providers: [
+    HeroService,
   ],
   bootstrap: [ AppComponent ]
 })
+
 export class AppModule { }
