1b8417c11557f406d59e0ec6fa1903724d29b3b2
Hetty de Vries <hetty82@gmail.com>
2017-02-15 12:42:48 +0100

    Multiple Components
    - and remove system.js files from gitignore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,5 @@
 import { Component } from '@angular/core';
-
-export class Hero {
-  id: number;
-  name: string;
-}
+import { Hero } from './hero';
 
 const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
@@ -17,7 +13,6 @@ const HEROES: Hero[] = [
   { id: 19, name: 'Magma' },
   { id: 20, name: 'Tornado' }
 ];
-
 
 @Component({
   selector: 'my-app',
@@ -33,17 +28,8 @@ const HEROES: Hero[] = [
       </li>
     </ul>
 
-    <div *ngIf="selectedHero">
-      <h2>{{selectedHero.name}} details!</h2>
-      <div>
-        <label>id: </label>{{selectedHero.id}}
-      </div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="selectedHero.name" placeholder="name" >
-      </div>
-    </div>
-    `,
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
+   `,
   styles: [`
     .selected {
       background-color: #CFD8DC !important;
@@ -104,4 +90,3 @@ export class AppComponent  {
     this.selectedHero = hero;
   }
 }
-
